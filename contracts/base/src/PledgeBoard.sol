14f5b9481d59c62ea18586695f8f30696089a545
solidworkssa <solidworkssa@gmail.com>
2026-02-19 05:38:23 +0100

    fix: add author attribution and version to PledgeBoard contract

--- contracts/base/src/PledgeBoard.sol
+++ contracts/base/src/PledgeBoard.sol
@@ -2,8 +2,11 @@
 pragma solidity ^0.8.20;
 
 /// @title PledgeBoard Contract
+/// @author solidworkssa
 /// @notice Community pledge tracker for crowdfunding.
 contract PledgeBoard {
+    string public constant VERSION = "1.0.0";
+
 
     mapping(address => uint256) public pledges;
     uint256 public goal;
